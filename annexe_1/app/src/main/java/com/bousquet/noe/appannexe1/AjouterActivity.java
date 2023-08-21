46e3c1336b405acc05bf197b144699a406d53b61
Sl1mmy <noebousquet2004@gmail.com>
2023-08-21 14:59:02 -0400

    commit

---
+++ annexe_1/app/src/main/java/com/bousquet/noe/appannexe1/AjouterActivity.java
@@ -0,0 +1,14 @@
+package com.bousquet.noe.appannexe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjouterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter);
+    }
+}
