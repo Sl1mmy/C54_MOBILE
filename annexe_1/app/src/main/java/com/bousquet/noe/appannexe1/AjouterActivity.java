e913c6a49c425958d3f1358314c0fb4c6f69b697
Sl1mmy <noebousquet2004@gmail.com>
2023-08-21 16:12:46 -0400

    commit

--- annexe_1/app/src/main/java/com/bousquet/noe/appannexe1/AjouterActivity.java
+++ annexe_1/app/src/main/java/com/bousquet/noe/appannexe1/AjouterActivity.java
@@ -2,13 +2,70 @@ package com.bousquet.noe.appannexe1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class AjouterActivity extends AppCompatActivity {
+    Button ajouterButton;
+    TextView texte;
+    Ecouteur ec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter);
+
+        ajouterButton = findViewById(R.id.buttonAjouterTexte);
+        texte = findViewById(R.id.addEntry);
+        Ecouteur ec = new Ecouteur();
+
+        ajouterButton.setOnClickListener(ec);
+    }
+
+    private class Ecouteur implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+
+            FileOutputStream fos;
+            BufferedWriter bw = null;
+            try {
+                //pour ecrire vers un fichier
+                fos = openFileOutput("memos.txt", Context.MODE_APPEND); //append ecrit a la fin du fichier
+                //tranforme le data en characters aulieux de binaire
+                OutputStreamWriter osw = new OutputStreamWriter(fos);
+                bw = new BufferedWriter(osw);
+                bw.write(texte.getText().toString());
+                bw.newLine();
+
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                fermerFlux(bw);
+            }
+
+            finish();
+        }
+    }
+
+    public void fermerFlux (Writer bw) {
+        try {
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- annexe_1/app/src/main/java/com/bousquet/noe/appannexe1/ListeActivity.java
+++ annexe_1/app/src/main/java/com/bousquet/noe/appannexe1/ListeActivity.java
@@ -3,12 +3,60 @@ package com.bousquet.noe.appannexe1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.util.ArrayList;
 
 public class ListeActivity extends AppCompatActivity {
+    ListView liste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste);
+        liste = findViewById(R.id.listText);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recupereMemo());
+        liste.setAdapter(adapter);
+
+    }
+
+    public ArrayList<String> recupereMemo() {
+        ArrayList<String> temp = new ArrayList<String>();
+
+        //3 flux de donnees en lecture
+        String line;
+
+        FileInputStream fis = null;
+        BufferedReader br = null;
+        try {
+            fis = openFileInput("memos.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+            do {
+                line = br.readLine();
+                temp.add(line);
+
+            } while (br.readLine() != null);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                assert br != null;
+                br.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return temp;
     }
 }
